7e7c656519877b87fef79aa23d78da715e550b26
linbian <linbian@yeah.net>
2020-03-19 10:18:42 +0800

    字典

---
+++ mywork/DictCode.py
@@ -0,0 +1,85 @@
+SYMBOL_JSON = {
+    "rb":{"sylbom":"RB0","name":"螺纹钢","contractSize":"10","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "ag":{"sylbom":"AG0","name":"白银","contractSize":"15","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "au":{"sylbom":"AU0","name":"黄金","contractSize":"1000","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "cu":{"sylbom":"CU0","name":"沪铜","contractSize":"5","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "al":{"sylbom":"AL0","name":"沪铝","contractSize":"5","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "zn":{"sylbom":"ZN0","name":"沪锌","contractSize":"5","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "pb":{"sylbom":"PB0","name":"沪铅","contractSize":"5","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "ru":{"sylbom":"RU0","name":"橡胶","contractSize":"10","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "fu":{"sylbom":"FU0","name":"燃油","contractSize":"10","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "sc":{"sylbom":"SC0","name":"原油","contractSize":"1000","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "bu":{"sylbom":"BU0","name":"沥青","contractSize":"10","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "hc":{"sylbom":"HC0","name":"热轧卷板","contractSize":"10","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "sn":{"sylbom":"SN0","name":"沪锡","contractSize":"1","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "ni":{"sylbom":"NI0","name":"沪镍","contractSize":"1","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "sp":{"sylbom":"SP0","name":"纸浆","contractSize":"10","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "nr":{"sylbom":"NR0","name":"20号胶","contractSize":"10","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "ss":{"sylbom":"SS0","name":"不锈钢","contractSize":"5","exchangeName":"上期所","exchangeCode":"SHFE"},
+    "wr":{"sylbom":"WR0","name":"线材","contractSize":"10","exchangeName":"上期所","exchangeCode":"SHFE"},  ## 上期所
+
+    "a":{"sylbom":"A0","name":"大豆一","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},  ## 豆一
+    "b":{"sylbom":"B0","name":"大豆二","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},  ## 豆一
+    "m":{"sylbom":"M0","name":"豆粕","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},
+    "y":{"sylbom":"Y0","name":"豆油","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},
+    "j":{"sylbom":"J0","name":"焦炭","contractSize":"60","exchangeName":"大连商品交易所","exchangeCode":"DCE"},
+    "c":{"sylbom":"C0","name":"玉米","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},
+    "l":{"sylbom":"L0","name":"聚乙烯","contractSize":"5","exchangeName":"大连商品交易所","exchangeCode":"DCE"},  ## 就是塑料？对，代码一致的
+    "pp":{"sylbom":"PP0","name":"聚丙烯","contractSize":"5","exchangeName":"大连商品交易所","exchangeCode":"DCE"},  ## 就是塑料？对，代码一致的
+    "fb":{"sylbom":"FB0","name":"纤维板","contractSize":"500","exchangeName":"大连商品交易所","exchangeCode":"DCE"},  ## 就是塑料？对，代码一致的
+    "p":{"sylbom":"P0","name":"棕油","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},  ## 就是棕榈油？对的，代码一致
+    "v":{"sylbom":"V0","name":"PVC","contractSize":"5","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+    "i":{"sylbom":"I0","name":"铁矿石","contractSize":"100","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+    "jd":{"sylbom":"JD0","name":"鸡蛋","contractSize":"100","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+    "bb":{"sylbom":"BB0","name":"胶合板","contractSize":"500","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+    "jm":{"sylbom":"JM0","name":"焦煤","contractSize":"60","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+    "cs":{"sylbom":"CS0","name":"玉米淀粉","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+    "eg":{"sylbom":"EG0","name":"乙二醇","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+    "rr":{"sylbom":"RR0","name":"粳米","contractSize":"10","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+    "eb":{"sylbom":"EB0","name":"苯乙烯","contractSize":"5","exchangeName":"大连商品交易所","exchangeCode":"DCE"},    ### 对的，代码v，又叫聚氯乙烯
+
+    "rs":{"sylbom":"RS0","name":"菜籽","contractSize":"10","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"}, ## 油菜籽-郑商所
+    "rn":{"sylbom":"RM0","name":"菜粕","contractSize":"10","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "fg":{"sylbom":"FG0","name":"玻璃","contractSize":"20","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "cf":{"sylbom":"CF0","name":"棉花","contractSize":"5","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    ##"ws":{"sylbom":"WS0","name":"强麦","contractSize":"20","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"}, ## 代码不一样呢和天勤的对比  待确定，郑商所代码与天勤是一致的
+    # "er":{"sylbom":"ER0","name":"籼稻","contractSize":"20"}, ## ？？？郑商所-早籼稻:RI,郑商所-晚籼稻:LR，就是没用ER
+    # "me":{"sylbom":"ME0","name":"甲醇","contractSize":"10","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"}, ## 郑商所是MA，没有me呢
+   ## "ro":{"sylbom":"RO0","name":"菜油","contractSize":"10","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"}, ## 代码不一样呢和天勤的对比
+    "ta":{"sylbom":"TA0","name":"甲酸","contractSize":"5","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},  ## 就是PTA？？是的
+    "oi":{"sylbom":"OI0","name":"菜油","contractSize":"10","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "zc":{"sylbom":"ZC0","name":"动力煤","contractSize":"100","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "wh":{"sylbom":"WH0","name":"强麦","contractSize":"20","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "jr":{"sylbom":"JR0","name":"粳稻","contractSize":"20","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "sr":{"sylbom":"SR0","name":"白糖","contractSize":"10","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "ri":{"sylbom":"RI0","name":"早籼稻","contractSize":"20","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "lr":{"sylbom":"LR0","name":"晚籼稻","contractSize":"20","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "ma":{"sylbom":"MA0","name":"甲醇","contractSize":"10","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "sf":{"sylbom":"SF0","name":"硅铁","contractSize":"5","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "sm":{"sylbom":"SM0","name":"锰硅","contractSize":"5","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "cy":{"sylbom":"CY0","name":"棉纱","contractSize":"5","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "ap":{"sylbom":"AP0","name":"鲜苹果","contractSize":"10","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "cj":{"sylbom":"CJ0","name":"红枣","contractSize":"5","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "ur":{"sylbom":"UR0","name":"尿素","contractSize":"20","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    "sa":{"sylbom":"SA0","name":"纯碱","contractSize":"20","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"},
+    # "pm":{"sylbom":"PM0","name":"普麦","contractSize":"50","exchangeName":"郑州商品交易所","exchangeCode":"CZCE"}, ## 新浪没有该项数据
+}
+
+
+INDEX_JSON={
+    "AGRI-OIL":{"name":"农产品-油脂油料","content":{"a":"大豆一","y":"豆油","m":"豆粕","oi":"菜油","p":"棕油","rn":"菜粕","rs":"油菜籽"}},
+    "AGRI-CANDY":{"name":"农产品-棉花糖","content":{"cf":"棉花","sr":"糖"}},
+    "AGRI-OTHERS":{"name":"农产品-其他农产品","content":{"c":"玉米","jd":"鸡蛋","cs":"玉米淀粉","ri":"早籼稻","lr":"晚籼稻","wh":"强麦","jr":"粳稻","ap":"苹果","cy":"棉纱"}},
+    "METAL-NOBLE":{"name":"金属-贵金属","content":{"au":"黄金","ag":"白银"}},
+    "METAL-COLOR":{"name":"金属-有色金属","content":{"cu":"铜","al":"铝","pb":"铅","zn":"锌","ni":"镍","sn":"锡"}},
+    "METAL-BLACK":{"name":"金属-黑色系","content":{"rb":"螺纹钢","i":"铁矿石","sm":"锰硅","hc":"热轧卷板","wr":"线材","sf":"硅铁"}},
+    "ENERGY-COAL":{"name":"能化-煤炭","content":{"zc":"动力煤","j":"焦炭","jm":"焦煤"}},
+    "ENERGY-OTHERS":{"name":"能化-其他能化","content":{"ru":"天然橡胶","sc":"原油","l":"聚乙烯","ta":"PTA","pp":"聚丙烯","fg":"玻璃","bu":"沥青","v":"PVC","ma":"甲醇","fu":"燃油"}},
+    "INDUSTRY-ESTATE":{"name":"工业品-黑色产业","content":{"rb":"螺纹钢","i":"铁矿石","jm":"焦煤","j":"焦炭","hc":"热轧卷板","wr":"线材","sm":"锰硅","sf":"硅铁"}},
+    "INDUSTRY-MATERAIL":{"name":"工业品-黑色原材料","content":{"i":"铁矿石","jm":"焦煤","j":"焦炭","sf":"硅铁","sm":"锰硅"}},
+    "INDUSTRY-BUILDING":{"name":"工业品-建材","content":{"fg":"玻璃","rb":"螺纹钢","v":"PVC","bb":"胶合板","fb":"纤维板"}},
+
+}
+
+def ts():
+    print("xxx")
